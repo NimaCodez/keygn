28ac0be5cb1f3c1358a0defa4b447e3c462c161c
Nima <nimacodes@gmail.com>
2024-02-06 18:55:08 +0330

    :zap: Done V1.0.1

--- bin/index.js
+++ bin/index.js
@@ -1,21 +1,82 @@
-#! /usr/bin/env node
+#!/usr/bin/env node
 
-const { program } = require("commander");
-const { randomBytes } = require("crypto");
+import * as p from "@clack/prompts";
+import chalk from "chalk";
+import { randomBytes } from "node:crypto";
 
-program.parse(process.argv);
-const options = program.opts();
-console.log(options);
+async function main() {
+	p.intro(
+		`${chalk.bgBlue(
+			chalk.white(" KEYGEN.JS "),
+		)}`,
+	);
 
-program
-	.command("keygn", "Generate random key")
-	.option("-bytes, --bytes <bytes>", "Number of bytes", 64)
-	.action(() => {
-		randomBytes(64, (err, buffer) => {
-			if (err) throw err;
-			else console.log(buffer.toString("hex"));
-		});
+	const length = await p
+		.text({
+			message: "How many bytes you want your key to be?",
+			defaultValue: 64,
+			placeholder: "Press [Enter] if you want to use default (64 bytes)",
+			validate: value => {
+				if (isNaN(+value)) {
+					console.log('value: ', value)
+					return chalk.bgRed(chalk.black("Please enter a number"));
+				}
+			},
+		})
+		
+	const type = await p
+	.select({
+		message: "Which type you want your key to be?",
+		defaultValue: 'hex',
+		options: [
+			{
+				label: 'Hexadecimal',
+				hint: 'hex',
+				value: 'hex',
+			},
+			{
+				label: 'Base64',
+				hint: 'base64',
+				value: 'base64',
+			},
+			{
+				label: 'Base64url',
+				hint: 'base64url',
+				value: 'base64url',
+			},
+			{
+				label: 'UTF-8',
+				hint: 'utf-8',
+				value: 'utf-8',
+			},
+			{
+				label: 'ASCII',
+				hint: 'ascii',
+				value: 'ascii',
+			},
+			{
+				label: 'Binary',
+				hint: 'binary',
+				value: 'binary',
+			},
+			{
+				label: 'UCS2',
+				hint: 'ucs2',
+				value: 'ucs2',
+			}
+		],
+		initialValue: 'hex'
+	})
+	
+	randomBytes(+length, async (err, buffer) => {
+		if (err) throw err;
+		else process.stdout.end(`YOUR KEY: ${chalk.green(buffer.toString(type))}`);
 	});
 
+	p.outro(
+		`${chalk.bgBlue(
+			chalk.white(" KEYGEN.JS "),
+		)} Made by NimaCodez.`,
+	);}
 
-program.parse()
+main();
